27ccd323a9991ca909124b0345e44736367e9786
Muhammad Faizan Khan <mfkhan2020@yahoo.com>
2024-12-27 16:24:03 +0500

    Add files via upload

---
+++ trello app/main.js
@@ -0,0 +1,10 @@
+let data_div = document.getElementById("data-div");
+
+
+function add_task(){
+    let dyn_div = document.createElement("div");
+    let dyn_head = document.createElement("input");
+
+    data_div.appendChild.dyn_div;
+    dyn_div.appendChild.dyn_head;
+}
